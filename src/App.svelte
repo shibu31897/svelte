bb7ba0ef79c4ae0991ac730ab2a1b06c2bbf54e4
Harshit Gupta <harshitg31897@outlook.com>
2024-08-28 14:04:51 +0530

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+	export let name;
+	export let age;
+
+	function incrementAge(){
+		age+=1
+	}
+
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
+
+<h1>Hello {name} aged {age}!</h1>
