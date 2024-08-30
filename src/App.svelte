09e43a8c22a973ad85a65c74a45231e1a9569548
Harshit Gupta <harshitg31897@outlook.com>
2024-08-30 15:41:41 +0530

    using multiple components

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,5 @@
 <script>
+	import ContactCard from "./ContactCard.svelte";
 	let name='Harshit';
 	let age = 30;
 	// let upperCaseName not required
@@ -33,3 +34,4 @@
 <!-- <button on:click={changeName}>Change Name</button> -->
  <!-- <input value="{name}" on:input="{nameInput}"> to achive this 2 way binding we can use -->
  <input type="text" bind:value={name}>
+ <ContactCard/>
